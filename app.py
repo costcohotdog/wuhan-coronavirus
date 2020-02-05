b8466c3fc78f63c365ad03ce330f2c93479f0bff
Terra Thompson <Terra925@gmail.com>
2020-02-05 12:27:28 -0800

    fixed names of functions in app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from pymongo import MongoClient
 from bson.json_util import dumps
 
 app = Flask(__name__)
-username = 'Terra925'
-password = 'H%40mmond271'
+username = 'USERNAME HERE'
+password = 'PASSWORD HERE'
 client = MongoClient("mongodb+srv://" + username + ":" + password + "@cluster0-paegd.mongodb.net/test?retryWrites=true&w=majority")
 db = client.corona_virus
 
@@ -18,12 +18,12 @@ def home():
 
 
 @app.route('/api/cases',methods=['GET'])
-def api_cases():
+def api_case():
     case_list = list(cases_collection.find())
     return current_app.response_class(dumps(case_list), mimetype="application/json")
 
 @app.route('/api/cases/<location>',methods=['GET'])
-def api_cases_location(location):
+def api_case_location(location):
     case_list = list(cases_collection.find({'location': location}))
     return current_app.response_class(dumps(case_list), mimetype="application/json")
 
