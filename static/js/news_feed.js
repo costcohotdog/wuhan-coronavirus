72accf328bdc6565658ed001d57de3985239b61f
Terra Thompson <Terra925@gmail.com>
2020-02-19 19:28:25 -0800

    Turned news feed into slideshow

--- static/js/news_feed.js
+++ static/js/news_feed.js
@@ -10,60 +10,29 @@ function updateNewsFeed(obj) {
     headline = obj[property].headline;
     headlines.push(headline);
   }
-  let timeline = {
-    dates: dates,
-    headlines: headlines
-  }
-
-  div = d3.select(".timeline").append('div')
-  let article, story;
-  for (i=0; i < dates.length; i++) {
-    article = div.append("h3").text(`${dates[i]}`).classed('story-date', true).classed('text-center', true).attr('id', `story-date${i}`)
-              .on('mouseover', function() {
-                d3.select(this).style('color', 'darkOrange')
-              })
-              .on('mouseout', function() {
-                d3.select(this).style('color', 'white')
-              });
-    story = article.append('h4').text(`${headlines[i]}`).classed('headline', true).classed('text-center', true).attr('id', `headline${i}`);
-  };
-
-
 
 
   for (let i=0; i < dates.length; i++) {
-    if (i >= 1) {
-      document.getElementById(`headline${i}`).style.display = 'none';
-      document.getElementById(`story-date${i}`).style.display = 'none';
+    let carousel = d3.select('.carousel-inner');
+    let random = Math.floor(Math.random() * Math.floor(9));
+    let div;
+    if (i === 0) {
+      div = carousel.append('div').classed('carousel-item active slide img-fluid', true);
     } else {
-      document.getElementById(`headline${i}`).style.display = 'block';
-      document.getElementById(`story-date${i}`).style.display = 'block';
+      div = carousel.append('div').classed('carousel-item slide img-fluid', true);
     }
+
+
+    div.html(`
+      <img class="d-block w-100" src="../static/images/img${random}.jpg" alt="..." style=" opacity: .5;">
+      <div class="carousel-caption d-none d-md-block">
+      <h3>${dates[i]}</h3>
+      <h5>${headlines[i]}</h5>
+      </div>`)
+
   }
 
-  let button = d3.select('#show-more')
-  let buttonText;
-  button.on('click', function() {
-    buttonText = button.text()
-    if (buttonText === 'Show More') {
-      for (let i=0; i < dates.length; i++) {
-        document.getElementById(`headline${i}`).style.display = 'block';
-        document.getElementById(`story-date${i}`).style.display = 'block';
-        button.text('Show Less');
-      }
-    } else {
-      for (let i=0; i < dates.length; i++) {
-        if (i >= 1) {
-          document.getElementById(`headline${i}`).style.display = 'none';
-          document.getElementById(`story-date${i}`).style.display = 'none';
-          button.text('Show More');
-        }
-      }
-    }
-    document.getElementById('timeline').scrollIntoView();
 
-
-  })
 
 
 
